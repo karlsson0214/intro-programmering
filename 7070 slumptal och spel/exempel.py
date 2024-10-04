c023a62f8b2afef4b6696c8e590bfa588237f5bb
Karlsson0214 <karlsson0214@gmail.com>
2024-10-04 16:02:40 +0200

    nytt

---
+++ 7070 slumptal och spel/exempel.py
@@ -0,0 +1,8 @@
+import random
+
+i = 0
+while i < 20:
+    tal = random.random() * 50 + 50
+    print(tal)
+    i = i + 1
+
---
+++ 7070 slumptal och spel/exempel_1.py
@@ -0,0 +1,7 @@
+# pseudokod
+# steg 1
+# slumpa två tal mellan 1 och 6
+# skriv ut talen
+# om skillnade mellan talen är två skriv ut: "Skillnaden är två"
+# steg 2
+# upprepa så länge användaren vill
